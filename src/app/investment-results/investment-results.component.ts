0020a8900b620dcd73407c9a88e7414135e3e1db
Jmar33 <josimarferreira922@yahoo.com.br>
2024-12-31 12:53:52 -0300

    feat: showing investment results

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,20 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { InvestmentSerivce } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+
+  private investmentService = inject(InvestmentSerivce);
+
+  get results(){
+    return this.investmentService.resultData;
+  }
+
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentSerivce } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -9,13 +10,25 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  enteredInitialInvestment = '0';
-  enteredAnnualInvestment = '0';
-  enteredExpectedReturn = '5';
-  enteredDuration = '10';
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  constructor(private investmentService: InvestmentSerivce){}
 
   onSubmit(){
-    console.log('enviei')
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: + this.enteredInitialInvestment(),
+      annualInvestment: + this.enteredAnnualInvestment(),
+      expectedReturn: + this.enteredExpectedReturn(),
+      duration: + this.enteredDuration()
+    })
+
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
   }
 
 }
